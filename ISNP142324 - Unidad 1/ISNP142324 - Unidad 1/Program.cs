1b318993f8738eadab2efdc4a2c8c6d2ff2627d7
Carlos Ruiz <mastermajora@protonmail.com>
2024-10-18 19:41:44 -0600

    Parte que solicita opción a ejecutar.

---
+++ ISNP142324 - Unidad 1/ISNP142324 - Unidad 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISNP142324___Unidad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seleccione la opción que desea ejecutar: ");
+            Console.WriteLine("1- Validador de números positivos o negativos");
+            Console.WriteLine("2- Tablas de multiplicar");
+            Console.WriteLine("3- Sumatoria de números positivos");
+            Console.WriteLine("4- Validador de números primos");
+            Console.WriteLine("5- Prmedio de serie de números");
+        }
+    }
+}
